03deebf17c5d02df998da4e32d0ed25b4129bf54
raulmendes-imob <raulcalixtoimob@gmail.com>
2025-12-26 15:36:06 -0300

    Update guard.js

--- app/js/guard.js
+++ app/js/guard.js
@@ -1,15 +1,9 @@
-// guard.js
-// Proteção de páginas privadas
-
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const auth = window.firebaseAuth;
 
-// Aguarda o estado de autenticação
 onAuthStateChanged(auth, (user) => {
   if (!user) {
-    // Usuário não logado → volta para login
-   window.location.href = "/simulador-imob/app/login.html";
+    window.location.href = "/simulador-imob/app/login.html";
   }
-  // Se estiver logado, segue normalmente
 });
